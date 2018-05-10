638b41c760b400162a0c5d665186c143c38a5348
nehakeshan <nkeshan@syr.edu>
2018-05-10 16:13:21 -0400

    initial commit

--- homework3.py
+++ homework3.py
@@ -104,6 +104,8 @@ def process_data():
     build_vocab("train.vi.txt")
     token2id('train', 'en')
     token2id('train', 'vi')
+    token2id('tst2012', 'en')
+    token2id('tst2012', 'vi')
 
 
 def load_vocab(vocab_path):
@@ -165,7 +167,7 @@ def _get_buckets():
     train_buckets_scale is the inverval that'll help us
     choose a random bucket later on.
     """
-    test_buckets = load_data('tst2012_ids.en', 'tst2012_ids.vi')
+    test_buckets = load_data('test_ids.en', 'test_ids.vi')
     data_buckets = load_data('train_ids.en', 'train_ids.vi')
     train_bucket_sizes = [len(data_buckets[b]) for b in range(len(BUCKETS))]
     print("Number of samples in each bucket:\n", train_bucket_sizes)
