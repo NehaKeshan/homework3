eb06cfe4019c9aa74cfc9f45dd676beb4a1a1e5d
nehakeshan <nkeshan@syr.edu>
2018-05-10 16:04:34 -0400

    initial commit

--- homework3.py
+++ homework3.py
@@ -165,7 +165,7 @@ def _get_buckets():
     train_buckets_scale is the inverval that'll help us
     choose a random bucket later on.
     """
-    test_buckets = load_data('tst2012_ids.en', 'tst2012_ids.vi')
+    test_buckets = load_data('tst2012_ids.en.txt', 'tst2012_ids.vi.txt')
     data_buckets = load_data('train_ids.en', 'train_ids.vi')
     train_bucket_sizes = [len(data_buckets[b]) for b in range(len(BUCKETS))]
     print("Number of samples in each bucket:\n", train_bucket_sizes)
